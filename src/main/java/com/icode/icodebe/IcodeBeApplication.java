090522ec32948d5ecc0a064895b78d0fb1e6b93d
ciordan <Catalin.Iordan@endava.com>
2020-10-10 19:46:47 +0300

    Initial commit

---
+++ src/main/java/com/icode/icodebe/IcodeBeApplication.java
@@ -0,0 +1,13 @@
+package com.icode.icodebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcodeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcodeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icode/icodebe/IcodeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icode.icodebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcodeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
