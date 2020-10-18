4fb2708549179d3098142559761ed28a47198470
ciordan <Catalin.Iordan@endava.com>
2020-10-18 19:25:58 +0300

    #30: Create the functionality to read a file from disk

--- src/main/java/com/icode/icodebe/router/CodeExecutionRouterConfig.java
+++ src/main/java/com/icode/icodebe/router/CodeExecutionRouterConfig.java
@@ -5,6 +5,9 @@ import com.icode.icodebe.model.ExecutionResult;
 import com.icode.icodebe.service.CodeExecutionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
@@ -22,14 +25,25 @@ public class CodeExecutionRouterConfig {
 
     @Bean
     public RouterFunction<ServerResponse> executionRouter() {
-        return route(POST("/api/execute-code"), this::handleExecute);
+        return route(POST("/api/execute-code"), this::handleExecute)
+                .andRoute(GET("/api/execution-result/{directoryName}"), this::handleGetExecutionResult);
     }
 
     private Mono<ServerResponse> handleExecute(ServerRequest serverRequest) {
-       final var responsePub =  serverRequest.bodyToMono(ExecutionRequest.class)
+        final var responsePub = serverRequest.bodyToMono(ExecutionRequest.class)
                 .map(codeExecutionService::createHTMLContent)
                 .flatMap(codeExecutionService::writeContent);
 
         return ServerResponse.ok().body(responsePub, ExecutionResult.class);
     }
+
+    private Mono<ServerResponse> handleGetExecutionResult(ServerRequest serverRequest) {
+        final var directoryName = serverRequest.pathVariable("directoryName");
+        final var responsePub = codeExecutionService.getExecutionResult(directoryName)
+                .map(InputStreamResource::new);
+
+        return ServerResponse.ok().contentType(MediaType.TEXT_HTML)
+                .body(responsePub, Resource.class);
+    }
+
 }
