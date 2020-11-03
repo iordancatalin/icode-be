d999f00b07efe9aa29a31fa8b4fd03dea52d6da5
ciordan <Catalin.Iordan@endava.com>
2020-11-03 23:39:31 +0200

    #83: Add validation logic

--- src/main/java/com/icode/icodebe/model/request/RequestResetPassword.java
+++ src/main/java/com/icode/icodebe/model/request/RequestResetPassword.java
@@ -2,7 +2,11 @@ package com.icode.icodebe.model.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class RequestResetPassword {
+
+    @NotBlank
     private String input;
 }
--- src/main/java/com/icode/icodebe/model/request/ResetPassword.java
+++ src/main/java/com/icode/icodebe/model/request/ResetPassword.java
@@ -2,8 +2,11 @@ package com.icode.icodebe.model.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class ResetPassword {
 
+    @NotBlank
     private String newPassword;
 }
