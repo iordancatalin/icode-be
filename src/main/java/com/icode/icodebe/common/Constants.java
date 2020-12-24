90045f100536a034eaebf4896013f2e3e37e9a9d
ciordan <Catalin.Iordan@endava.com>
2020-12-24 23:17:12 +0200

    Implement theme selection

--- src/main/java/com/icode/icodebe/common/Constants.java
+++ src/main/java/com/icode/icodebe/common/Constants.java
@@ -3,8 +3,7 @@ package com.icode.icodebe.common;
 public class Constants {
     public static final String AUTH_TOKEN_TYPE = "Bearer ";
 
-    public static final String STORAGE_ROOT_DIRECTORY = System.getProperty("user.home") + "\\icode";
-//    public static final String STORAGE_ROOT_DIRECTORY = "/data/icode";
+    public static final String STORAGE_ROOT_DIRECTORY;
 
     public static final String FILE_TEMPLATE = "<html lang=\"en\">\n" +
             "  <head>\n" +
@@ -16,4 +15,18 @@ public class Constants {
             "    %s<div><script>%s</script></div>\n" +
             "  </body>\n" +
             "</html>\n";
+
+    static {
+        final var OS = System.getProperty("os.name").toLowerCase();
+
+        if (isUnix(OS)) {
+            STORAGE_ROOT_DIRECTORY = "/data/icode";
+        } else {
+            STORAGE_ROOT_DIRECTORY = System.getProperty("user.home") + "/icode";
+        }
+    }
+
+    private static boolean isUnix(String OS) {
+        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
+    }
 }
